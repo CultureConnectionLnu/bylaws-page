14c870dc9f65726caaa25872dc7d42bb7c203207
Arturyux <artur.burlakin@gmail.com>
2024-12-17 22:09:56 +0100

    Install react-markdown, adding test mardonw to json file

--- src/Bylaws.jsx
+++ src/Bylaws.jsx
@@ -1,19 +1,12 @@
-import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import BylawsText from './assets/bylaws.json';
 
 function Bylaws() {
-  const [markdownContent, setMarkdownContent] = useState('');
-
-  useEffect(() => {
-    fetch('/bylaws.md')
-      .then((res) => res.text())
-      .then((text) => setMarkdownContent(text));
-  }, []);
 
   return (
     <div className="flex justify-center mt-6">
       <div className="bg-white/80 rounded-lg p-6 max-w-xl w-full shadow-md h-96 overflow-y-auto">
-        <ReactMarkdown>{markdownContent}</ReactMarkdown>
+      <ReactMarkdown>{BylawsText .markdown}</ReactMarkdown>
       </div>
     </div>
   );
